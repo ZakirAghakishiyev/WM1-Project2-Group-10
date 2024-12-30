43479d5c009834f331eb37a16902f644169e23ff
ZakirAghakishiyev <118535508+ZakirAghakishiyev@users.noreply.github.com>
2024-12-30 21:13:34 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import RecipePage from "./pages/RecipePage";
+import Navbar from "./pages/Navbar";
+import Detail from './pages/Detail';
+import ContactMe from './pages/ContactMe';
 
 
 function App() {
@@ -9,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<RecipePage />} />
+        <Route path="/details/:id" element={<Detail />} />
+        <Route path="/contactMe" element={<ContactMe />} />
       </Routes>
     </Router>
   );
